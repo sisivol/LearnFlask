5e130d7e1b1abb13a08d6fe29735d2993b13fa5d
cs <sisivol@gmail.com>
2019-03-12 14:56:49 +0800

    '1'

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,22 @@ def index():
 @app.route('/hello')
 def hello():
     return 'hello Page'
-    
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(username)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login',next='/'))
+    print(url_for('profile',username='cao si'))
+
+
+
 if __name__ == '__main__':
     app.run()
